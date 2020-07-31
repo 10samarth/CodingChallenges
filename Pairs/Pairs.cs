f6abbee270093b1e4b926e32b3efdf44eb106d81
Samarth Goudar <51813032+10samarth@users.noreply.github.com>
2020-07-31 22:26:03 +0530

    Add files via upload

---
+++ Pairs/Pairs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenges
+{
+    class Pairs
+    {
+        public static int pairs(int k, int[] arr)
+        {
+            int count = 0;
+            HashSet<int> ht = new HashSet<int>(arr);
+
+            for(int i=0;i<arr.Length;i++)
+            {
+                if(ht.Contains(arr[i]-k))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CodingChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nk = "5 2".Split(' ');//Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[] arr = Array.ConvertAll("1 5 3 4 2".Split(' '), arrTemp => Convert.ToInt32(arrTemp));//Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int result = Pairs.pairs(k, arr);
+
+            Console.WriteLine(result);
+        }
+    }
+}
