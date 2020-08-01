17383996c523e04d54324da0f34a17b421360ae7
samarthg95 <samarthmg@gmail.com>
2020-08-01 23:52:22 +0530

    added challenge NewYear Chaos

---
+++ NewYear Chaos/NewYearChaos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingChallenges
+{
+    class NewYearChaos
+    {
+        public static void minimumBribes(int[] q)
+        {
+            int moves = 0;
+
+            for (int i = 0; i < q.Length; i++)
+            {
+                for (int j = Math.Max(0, q[i] - 2); j < i; j++)
+                {
+                    if (q[j] > q[i])
+                    {
+                        moves++;
+                    }
+                }
+
+                if ((q[i] - (i + 1)) > 2)
+                {
+                    Console.WriteLine("Too chaotic");
+
+                    return;
+                }
+            }
+
+            Console.WriteLine(moves);
+
+            return;
+
+        }
+
+    }
+}
