647c57a93a1e5c3954b2bb6cf201f7e1e1bab823
samarthg95 <samarthmg@gmail.com>
2020-08-04 20:52:02 +0530

    added SingleNumber Challenge

---
+++ SingleNumber/SingleNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodingChallenges.SingleNumber
+{
+    class SingleNumber
+    {
+        public static int singleNumber(int[] nums)
+        {
+            HashSet<int> hs = new HashSet<int>();
+
+            foreach(int number in nums)
+            {
+                if(hs.Contains(number))
+                {
+                    hs.Remove(number);
+                }
+                else
+                {
+                    hs.Add(number);
+                }
+            }
+
+            if(hs.Count>0)
+                return hs.First();
+
+            return 0;
+        }
+    }
+}
